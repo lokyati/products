d12dd8908afbe300b61274c409b9b34d3f0f2b02
lokyati <lokyati15@gmail.com>
2019-01-31 03:01:30 +0200

    resource

--- app/Http/Resources/ProductsResource.php
+++ app/Http/Resources/ProductsResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class StudentResource extends JsonResource
+class ProductsResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -17,8 +17,8 @@ class StudentResource extends JsonResource
         return [
             'id'=> $this->id,
             'nev' => $this->nev,
-            'szak' => $this->szak,
-            'evfolyam' => $this->evfolyam
+            'tipus' => $this->tipus,
+            'ar' => $this->ar
         ]; 
     }
 }
